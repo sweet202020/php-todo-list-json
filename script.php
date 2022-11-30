c451495107361f94ef4f7fe2ac2f7d01996ccd84
sweet202020 <dolcealessio1@gmail.com>
2022-11-30 17:21:40 +0100

    add tasks in file json

--- script.php
+++ script.php
@@ -1,10 +1,14 @@
 <?php
-$ToDo = [
+/* $ToDo = [
     'learn html',
     'learn css',
     'learn boostrap',
     'learn js'
-];
+]; */
 
+$ToDo_string = file_get_contents('list_tasks.json');
+/* var_dump($ToDo_string); */
+$ToDo = json_decode($ToDo_string);
+/* var_dump($ToDo); */
 header('Content-Type: application/JSON');
 echo json_encode($ToDo);
